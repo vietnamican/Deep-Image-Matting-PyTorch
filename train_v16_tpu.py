29f43b47b14f7e45ead65ca15ff9a1d46f7443a8
vietnamican <vietnamican@gmail.com>
2020-05-18 20:06:05 +0700

    nothing

--- train_v16_tpu.py
+++ train_v16_tpu.py
@@ -50,10 +50,11 @@ def train_net(index, args):
 
     # Custom dataloaders
     if xm.is_master_ordinal():
-        train_dataset = DIMDataset('train')
-        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=8)
-        valid_dataset = DIMDataset('valid')
-        valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=args.batch_size, shuffle=False, num_workers=8)
+        xm.rendezvous("init")
+    train_dataset = DIMDataset('train')
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=8)
+    valid_dataset = DIMDataset('valid')
+    valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=args.batch_size, shuffle=False, num_workers=8)
 
     # Epochs
     for epoch in range(start_epoch, args.end_epoch):
