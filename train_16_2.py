f6c3906512a712de32c7966431d890801095e905
vietnamican <vietnamican@gmail.com>
2020-05-20 21:59:43 +0700

    nothing

--- train_16_2.py
+++ train_16_2.py
@@ -47,9 +47,9 @@ def train_net(args):
 
     # Custom dataloaders
     train_dataset = DIMDataset('train')
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=16)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=8)
     valid_dataset = DIMDataset('valid')
-    valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=args.batch_size, shuffle=False, num_workers=16)
+    valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=args.batch_size, shuffle=False, num_workers=8)
 
     # Epochs
     for epoch in range(start_epoch, args.end_epoch):
