ef373bda36d79068c54d85e0de2f1165a693e488
vietnamican <vietnamican@gmail.com>
2020-05-26 15:24:01 +0700

    nothing

--- eval_trainval.py
+++ eval_trainval.py
@@ -14,7 +14,7 @@ from utils import ensure_folder, compute_mse, compute_sad, draw_str
 IMG_FOLDER = 'alphamatting/input_lowres'
 ALPHA_FOLDER = 'alphamatting/gt_lowres'
 TRIMAP_FOLDERS = ['alphamatting/trimap_lowres/Trimap1', 'alphamatting/trimap_lowres/Trimap2']
-OUTPUT_FOLDERS = ['alphamatting/output_lowres_new1/Trimap1', 'alphamatting/output_lowres_new1/Trimap2', 'images/alphamatting/output_lowres_new1/Trimap3', ]
+OUTPUT_FOLDERS = ['alphamatting/output_lowres_new2/Trimap1', 'alphamatting/output_lowres_new2/Trimap2', 'images/alphamatting/output_lowres_new2/Trimap3', ]
 
 if __name__ == '__main__':
     # checkpoint = 'BEST_checkpoint.tar'
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     # model = checkpoint['model'].module
     # model = model.to(device)
     # model.eval()
-    checkpoint = 'checkpoints_4/checkpoint_7_0.15485985313412234.tar'
+    checkpoint = 'checkpoints_1/checkpoint_4_0.06200232435071814.tar'
     checkpoint = torch.load(checkpoint)
     model = checkpoint['model']
     model = model.to(device)
