3b567f7b7bf8dea92bdd12c4fbfa846cebfd8143
vietnamican <vietnamican@gmail.com>
2020-05-29 22:04:16 +0700

    nothing

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ valid_ratio = 0.2
 # Training parameters
 num_workers = 1  # for data-loading; right now, only 1 works with h5py
 grad_clip = 5.  # clip gradients at an absolute value of
-print_freq = 50  # print training/validation stats  every __ batches
+print_freq = 100  # print training/validation stats  every __ batches
 checkpoint = None  # path to checkpoint, None if none
 
 ##############################################################
