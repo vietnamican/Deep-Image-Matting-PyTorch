45ca226675c5152e93c080d8eece058a87e31392
vietnamican <vietnamican@gmail.com>
2020-06-04 08:31:43 +0700

    nothing

--- data_gen_1_data_augumentation.py
+++ data_gen_1_data_augumentation.py
@@ -38,7 +38,7 @@ if args.data_augumentation:
 else:
     data_transforms = {
     'train': transforms.Compose([
-        # transforms.ColorJitter(brightness=0.125, contrast=0.125, saturation=0.125),
+        transforms.ColorJitter(brightness=0.125, contrast=0.125, saturation=0.125),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
     ]),
